fac2b064196be215726f5fdf64ae16487cab042b
Matheus M Almeida <matheus.mma2015@gmail.com>
2025-05-26 17:12:26 -0300

    pequenos ajustes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LoginComponent } from './shared/login/login.component';
 import { ManutencaoComponent } from './shared/manutencao/manutencao.component';
 import { RegistrarComponent } from './shared/registrar/registrar.component';
 import { PagamentoComponent } from './shared/pagamento/pagamento.component';
+import { ProductComponent } from './components/product/product.component';
 
 export const routes: Routes = [
   { path: 'entrar', component: LoginComponent },
   { path: 'manutencao', component: ManutencaoComponent },
   { path: 'registrar', component: RegistrarComponent },
   { path: 'pagamento', component: PagamentoComponent },
+  { path: 'produto', component: ProductComponent },
 ];
