db800610021ce4503115780808275f2bec87d112
VitorioAug <vito.augustodias@gmail.com>
2025-05-29 18:17:26 -0300

    Final

--- src/app/services/carrinho.service.ts
+++ src/app/services/carrinho.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, of } from 'rxjs';
 import { Produto } from './types/types';
 
+
 @Injectable({
   providedIn: 'root',
 })
--- src/app/shared/card-produto/card-produto.component.ts
+++ src/app/shared/card-produto/card-produto.component.ts
@@ -1,26 +1,38 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-export interface Produto {
-  preco: number;
-  id: any;
-  quantidade: any;
-  nome: string;
-  imagem: string;
-  precoOriginal: number;
-  precoAtual: number;
-  parcelas: string;
-  desconto: number;
-  freteGratis?: boolean;
-}
+import { RouterModule } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { ProdutosService } from '../../services/produtos.service';
 
 @Component({
   selector: 'app-card-produto',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './card-produto.component.html',
   styleUrls: ['./card-produto.component.css'],
 })
-export class ProductCardComponent {
-  @Input() produto!: Produto;
+export class ProdutostenisComponent implements OnInit {
+  produtostenis: any[] = [];
+
+  constructor(
+    private http: HttpClient,
+    private produtosService: ProdutosService
+  ) {}
+
+  ngOnInit(): void {
+    this.carregarProdutos();
+  }
+
+  calcularDesconto(precoAntigo: number, precoNovo: number): number {
+  const desconto = ((precoAntigo - precoNovo) / precoAntigo) * 100;
+  return Math.round(desconto);
+}
+
+  carregarProdutos(): void {
+  this.produtosService.listar().subscribe((data) => {
+    this.produtostenis = data.slice(0, 5);
+  });
+
+
+}
 }
